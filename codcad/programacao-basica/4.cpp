960d84cc0b274d043b25fbb52a1cd2d71f1c4b25
Jessica Cacau <jessicacacau@gmail.com>
2017-06-29 20:33:31 -0300

    Create 4.cpp

---
+++ codcad/programacao-basica/4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    double pedro, paulo;
+
+    cin >> pedro >> paulo;
+
+    if (pedro <= paulo)
+        cout << "Pedro\n";
+    else
+        cout << "Paulo\n";
+
+    return 0;
+}
