00bd6bc752bb9e583d8ba5ee6fcbf70dc80238e6
Jessica Cacau <jessicacacau@gmail.com>
2017-06-29 20:24:32 -0300

    Create 3.cpp

---
+++ codcad/programacao-basica/3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    double a, b;
+
+    cin >> a >> b;
+
+    cout.precision(2);
+    cout.setf(ios::fixed);
+
+    cout << a/b << "\n";
+
+    return 0;
+}
