d494eb49183808baaeeb6c503e74f12eaae49e4c
Jessica Cacau <jessicacacau@gmail.com>
2017-07-14 11:47:05 -0300

    Create 156.cpp

---
+++ codcad/programacao-basica/156.cpp
@@ -0,0 +1,24 @@
+#define __USE_MINGW_ANSI_STDIO 0
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+	ios_base::sync_with_stdio(false);
+	
+	int n, a, b=0, soma =0;
+	cin >> n;
+	
+	for (int i=0; i<n; i++) {
+		cin >> a;
+		soma += 10;
+		if (i!=0 and a-b<10 and a-b>0)
+			soma -= 10-(a-b);
+		
+		b = a;
+	}
+	
+	cout << soma << "\n";
+	
+	return 0;
+}
