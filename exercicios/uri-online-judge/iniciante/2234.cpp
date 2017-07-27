fb33fe4493907578a99dba2f5462ae238df87479
Jessica Cacau <jessicacacau@gmail.com>
2017-07-27 11:53:54 -0300

    Create 2234.cpp

---
+++ exercicios/uri-online-judge/iniciante/2234.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+	ios_base::sync_with_stdio(false);
+	
+	int a, b;
+	cin >> a >> b;
+	
+	cout << fixed << setprecision(2);
+	cout << (double)a/b << "\n";
+
+	return 0;
+}
