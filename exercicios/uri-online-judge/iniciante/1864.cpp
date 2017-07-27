022f4a0e04d781628284d7496e42981b8eaed001
Jessica Cacau <jessicacacau@gmail.com>
2017-07-27 11:50:22 -0300

    Create 1864.cpp

---
+++ exercicios/uri-online-judge/iniciante/1864.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+	ios_base::sync_with_stdio(false);
+
+	string a = "LIFE IS NOT A PROBLEM TO BE SOLVED";
+	int b;
+	
+	cin >> b;
+	for (int i=0; i<b; i++)
+		cout << a[i];
+	cout << "\n";
+
+	return 0;
+}
