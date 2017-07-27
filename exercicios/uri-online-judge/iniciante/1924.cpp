dd0c5b69aade1bdd4d5e2c6ed46ff5eeb152e342
Jessica Cacau <jessicacacau@gmail.com>
2017-07-27 11:48:36 -0300

    Create 1924.cpp

---
+++ exercicios/uri-online-judge/iniciante/1924.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+	ios_base::sync_with_stdio(false);
+
+	int n;
+	string a;
+	
+	cin >> n;
+	cin.ignore();
+	while (n--)
+		getline(cin, a);
+		
+	cout << "Ciencia da Computacao\n";
+
+	return 0;
+}
