53c77db5d999ad44a7a9807538f16328eaa38229
Jessica Cacau <jessicacacau@gmail.com>
2017-07-27 11:52:31 -0300

    Create 1984.cpp

---
+++ exercicios/uri-online-judge/iniciante/1984.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+	ios_base::sync_with_stdio(false);
+	
+	string a;
+	cin >> a;
+	
+	reverse(a.begin(), a.end());
+	cout << a << "\n";
+
+	return 0;
+}
