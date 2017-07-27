723b114dbc547ffc15355452d2a429dbc635dd9e
Jessica Cacau <jessicacacau@gmail.com>
2017-07-26 23:21:28 -0300

    Create 1930.cpp

---
+++ exercicios/uri-online-judge/iniciante/1930.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+	ios_base::sync_with_stdio(false);
+	
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	
+	cout << a+b+c+d-3 << "\n";
+	
+	return 0;
+}
