71a5dbd9dd4582fb6488e265cbd214caa6d4ba51
Jessica Cacau <jessicacacau@gmail.com>
2017-07-27 11:51:20 -0300

    Create 1963.cpp

---
+++ exercicios/uri-online-judge/iniciante/1963.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+	ios_base::sync_with_stdio(false);
+	
+	cout << fixed << setprecision(2);
+	
+	double a, b, dif, p;
+	cin >> a >> b;
+	
+	cout << ((b-a)*100.0)/a << "%\n";
+
+	return 0;
+}
