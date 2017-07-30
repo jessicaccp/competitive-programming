fce47a495109baec212c4e68220edae661302a19
Jessica Cacau <jessicacacau@gmail.com>
2017-07-30 18:10:24 -0300

    Create 2581.cpp

---
+++ exercicios/uri-online-judge/iniciante/2581.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+	ios_base::sync_with_stdio(false);
+	
+	int n;
+	string a;
+	
+	while (cin >> n) {
+		while (n--) {
+			cin >> a;
+			cout << "I am Toorg!\n";
+		}
+	}
+	
+	return 0;
+}
