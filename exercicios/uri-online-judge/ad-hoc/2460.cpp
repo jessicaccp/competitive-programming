d322e0e9e8af6bd22829aca3f01fc67a26be8dfc
Jessica Cacau <jessicacacau@gmail.com>
2017-08-01 10:11:27 -0300

    Create 2460.cpp

---
+++ exercicios/uri-online-judge/ad-hoc/2460.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+	ios_base::sync_with_stdio(false);
+	
+	int n, m, x, y;
+	vector<int> v;
+	vector<int>::iterator p;
+	
+	cin >> n;
+	while (n--) {
+		cin >> x;
+		v.push_back(x);
+	}
+	
+	cin >> m;
+	while (m--) {
+		cin >> y;
+		p = find(v.begin(), v.end(), y);
+		v.erase(p);
+	}
+	
+	for (int i=0; i<v.size(); i++)
+		if (i==0)
+			cout << v[i];
+		else
+			cout << " " << v[i];
+	cout << "\n";
+	
+	return 0;
+}
