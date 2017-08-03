1559cf1f834faa7d7c703eafaa95e6e5e4e13a8a
Jessica Cacau <jessicacacau@gmail.com>
2017-08-03 10:04:43 -0300

    Create ASCII Value.cpp

---
+++ exercicios/hacker-earth/code-monk/01-basics-of-programming/basics-of-input-output/ASCII Value.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+	ios_base::sync_with_stdio(false);
+	
+	char c;
+	int n;
+	
+	cin >> c;
+	n = c;
+	cout << n << "\n";
+	
+	return 0;
+}
