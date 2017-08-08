a4c390cda4ab09431fc1db78a3ee78b7f91b4ce9
Jessica Cacau <jessicacacau@gmail.com>
2017-08-07 22:45:43 -0300

    Create 1620.cpp

---
+++ exercicios/uri-online-judge/matematica/1620.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+	ios_base::sync_with_stdio(false);
+
+	double l, i, x;
+	
+	while (cin >> l and l) {
+		i = l+l-3;
+		x = (i-l)/l;
+		cout << fixed << setprecision(6) << x << "\n";
+	}
+	
+	return 0;
+}
